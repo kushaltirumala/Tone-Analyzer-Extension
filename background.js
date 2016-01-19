f8c066e497a9695cb3b85ab34bbfe27a6a207ac6
kushal tirumala <kushaltirumala99@gmail.com>
2016-01-18 23:07:50 -0800

    hibryanchen

--- background.js
+++ background.js
@@ -1,30 +1,32 @@
-var emojiMap = {
-	"happy":[":)", ":-)", ";)"]
-	"sad":"",
-	"slightlyhorny":[";)", ""],
-	"alexxuhorny":["<3", "luv<3"],
-	"loveydovey" :[":*", ""]
-	//whatever else
-}
+// var emojiMap = {
+// 	"happy":[":)", ":-)", ";)"]
+// 	"sad":"",
+// 	"slightlyhorny":[";)", ""],
+// 	"alexxuhorny":["<3", "luv<3"],
+// 	"loveydovey" :[":*", ""]
+// 	//whatever else
+// }
 
-function findEmojis(keyword) {
-	//return array of 3 possible emojis from emojiMap
-	var temp = emojiMap.keyword;
-	for(var i = 0; i < temp.length; i++) {
-		console.log(temp[i]);
-	}
-	return temp;
-}
+// function findEmojis(keyword) {
+// 	//return array of 3 possible emojis from emojiMap
+// 	var temp = emojiMap.keyword;
+// 	for(var i = 0; i < temp.length; i++) {
+// 		console.log(temp[i]);
+// 	}
+// 	return temp;
+// }
 
 
 function getFBText() {
 	return "HI GOT HERE";
 }
 
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "hello") {
-      alert("HI bryan chen");
-      sendResponse({farewell: "goodbye"});
-    }
-  });
+chrome.runtime.onMessage.addListener(
+   function(request, sender, sendResponse) {
+     if (request.type == "req") {
+       sendResponse({type: "test"});
+       text = request.text;
+       alert(text);
+     }
+ });
+ 
--- content.js
+++ content.js
@@ -1,30 +1,38 @@
-alert('hi');
 
-function suggest(text) {
-	//make XMLhttpreq and set tone into key
+// function suggest(text) {
+// 	//make XMLhttpreq and set tone into key
 	
-	var HTTPrequest = //url here
-    var req = new XMLHttpRequest();
-    //prbly post request tbh
-    req.open("GET", HTTPrequest, true);
-    req.onreadystatechange = function processData() {
-        if (req.readyState == 4) {
-            res = JSON.parse(req.responseText);
-            callback(tone);
-        }
-    }
-    req.send();
+// 	var HTTPrequest = //url here
+//     var req = new XMLHttpRequest();
+//     //prbly post request tbh
+//     req.open("GET", HTTPrequest, true);
+//     req.onreadystatechange = function processData() {
+//         if (req.readyState == 4) {
+//             res = JSON.parse(req.responseText);
+//             callback(tone);
+//         }
+//     }
+//     req.send();
 
-	var key = "happy";
-	findEmoji(key);
-}
+// 	var key = "happy";
+// 	//findEmoji(key);
+// }
+
+//alert('hi');
+
+
+
 
 var text = null;
 
 
-chrome.extension.sendRequest({greeting: "hello"}, function(response) {
-  alert(response.farewell);
-});
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+   chrome.tabs.sendMessage(tabs[0].id, {type: "req", text:"yoyo"}, function(response) {
+     if(response.type == "test"){
+       alert('test received');
+     }
+   });
+ });
 
 
 //suggest the thing to the user
